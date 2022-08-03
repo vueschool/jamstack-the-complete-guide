aff68f35c9108af9aa537dc1e845c9723f605bbf
Daniel Kelly <me@danielkelly.io>
2022-08-03 16:27:47 -0500

    9-fallback-content-for-new-projects

--- storyblok/Projects.vue
+++ storyblok/Projects.vue
@@ -6,13 +6,21 @@ defineProps({
 <template>
   <div class="container project-page">
     <div class="project-title">
-      <h1>{{ blok.name }}</h1>
+      <h1>{{ blok.name || "New Project" }}</h1>
       <NuxtLink :to="{ name: 'index' }">Go Back</NuxtLink>
     </div>
 
     <div class="project-description">
-      <img :src="blok.image.filename" :alt="blok.image.alt" />
-      <p>{{ blok.description }}</p>
+      <img
+        :src="blok.image?.filename || 'https://picsum.photos/300/175'"
+        :alt="blok.image?.alt"
+      />
+      <p>
+        {{
+          blok.description ||
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut accusantium perferendis molestias, dolorum, ea repellat doloremque, commodi corporis in omnis recusandae facilis laborum ipsum? Alias quos vitae facere aliquam incidunt."
+        }}
+      </p>
     </div>
   </div>
 </template>
