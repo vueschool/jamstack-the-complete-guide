299ec9fb960224241966001d3d9af2e15284f076
Daniel Kelly <me@danielkelly.io>
2022-08-16 13:11:00 -0500

    adjust to work with Nuxt 3 generate

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <script setup>
-const story = await useStoryblok(
-  "home",
-  {
+// data for static site gen
+
+const { data: story } = await useAsyncData("story", async ({}) => {
+  const story = await useStoryblok("home", {
     version: "draft",
     resolve_relations: "featuredProjects.projects",
-  },
-  {
-    resolveRelations: "featuredProjects.projects",
-  }
-);
+  });
+  return story.value;
+});
+
+// get the live previews working
+// onMounted(() => {
+//   useStoryblokBridge(
+//     story.value.id,
+//     (evStory) => {
+//       console.log(evStory);
+//       story.value = evStory;
+//     },
+//     {
+//       resolveRelations: "featuredProjects.projects",
+//     }
+//   );
+// });
 </script>
 <template>
-  <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+  <StoryblokComponent :blok="story.content"></StoryblokComponent>
 </template>
