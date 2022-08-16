81a19d864b426b1990392c8b9cfd13fc846ae567
Daniel Kelly <me@danielkelly.io>
2022-08-16 11:03:43 -0500

    featured projects update in preview

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <script setup>
-const story = await useStoryblok("home", {
-  version: "draft",
-  resolve_relations: "featuredProjects.projects",
-});
+const story = await useStoryblok(
+  "home",
+  {
+    version: "draft",
+    resolve_relations: "featuredProjects.projects",
+  },
+  {
+    resolveRelations: "featuredProjects.projects",
+  }
+);
 </script>
 <template>
   <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
